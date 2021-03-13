ab6eae5f319f631e9f17a0967c796806e879046e
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2021-03-13 10:44:58 +0530

    -

---
+++ LINKED_LIST/Nth_Node.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ar array
+const int mod = 1e9+7;
+const int mxN = 1e6;
+const int N = 2e5;
+
+int main() {
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+  
+  
+  
+return 0; 
+} 
